45f9edef335b81e50d8c5245190592ffcf7ebd5d
Lautaro Orazi <orazile@gmail.com>
2017-09-15 11:09:49 -0300

    Fix examples.

--- examples/migration.rb
+++ examples/migration.rb
@@ -6,21 +6,21 @@ CassandraSchema.migration(1) do
         name text,
         description text,
         PRIMARY KEY (id, name)
-      ) WITH CLUSTERING KEY ORDER BY (name ASC)
+      ) WITH CLUSTERING ORDER BY (name ASC)
     CQL
 
     execute <<~CQL
       CREATE MATERIALIZED VIEW table_by_description AS
         SELECT
-          id uuid,
-          name text,
-          description text
+          id,
+          name,
+          description
         FROM table_by_name
         WHERE id IS NOT NULL
           AND name IS NOT NULL
           AND description IS NOT NULL
         PRIMARY KEY (id, description, name)
-      ) WITH CLUSTERING KEY ORDER BY (description ASC, name ASC)
+        WITH CLUSTERING ORDER BY (description ASC, name ASC)
     CQL
   end
 
